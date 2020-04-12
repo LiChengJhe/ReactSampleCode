7e99e93d77c40e28b02bd65bf5cb71490c9b11ba
ChengJhe <chengjhe810824@gmail.com>
2020-04-12 23:35:30 +0800

    add files
    Signed-off-by: ChengJhe <chengjhe810824@gmail.com>

---
+++ tsmc/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import { Container } from '@material-ui/core';
+
+function App() {
+  return (
+  <Container>
+    <Button variant="contained" color="primary">
+      你好，世界
+    </Button>
+    <Button variant="contained" color="primary">
+      你好，世界
+      </Button>
+    <Button variant="contained" color="primary">
+      你好，世界
+        </Button>
+    <Button variant="contained" color="primary">
+      你好，世界
+          </Button>
+    <Button variant="contained" color="primary">
+      你好，世界
+            </Button>
+  </Container>
+  );
+}
+
+export default App;
