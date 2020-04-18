241235c11a7b30a0eeea959d3d1fa07917ef8b22
ChengJhe <chengjhe810824@gmail.com>
2020-04-18 17:44:03 +0800

    add
    Signed-off-by: ChengJhe <chengjhe810824@gmail.com>

--- covid-19/src/index.js
+++ covid-19/src/index.js
@@ -6,7 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+     <App />
+    {/* <Provider store={store}>
     <App />
+    </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
